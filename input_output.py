d2f4e7c1b0b030844d3dce82718d91bbcfcf0ba6
yogiparmana <49135804+yogiparmana@users.noreply.github.com>
2021-02-28 02:50:47 +0800

    Create input_output.py

---
+++ input_output.py
@@ -0,0 +1,18 @@
+print("Input Console\n")
+print("-----------------\n")
+
+#input menggunakan 
+#raw_input() string //untuk phyton 2.x
+#input() int (dan string untuk phyton 3.x)
+#number; #inisialisasi variable int number tidak perlu
+number = input("Masukkan Number : ") #keterangan input dan langsung  proses input
+print ("output number : {}\n".format(number)) # proses output
+
+
+#kata; # inisialisasi variable string kata tidak perlu
+kata = input("Masukkan Kata : ") #keterangan input dan langsung proses input
+print ("output string : ",kata) # output
+
+#sangat simple dan mudah di bandingkan yang lain
+
+   
